535b14392cb54f4b5c857d92aa27101e1b28edef
kuroniil <niilo.kuronen@helsinki.fi>
2025-11-29 12:27:44 +0200

    test change

--- src/components/puzzle_components/ModesTab.jsx
+++ src/components/puzzle_components/ModesTab.jsx
@@ -1,13 +1,20 @@
-const ModesTab = ({ handleExitClick, handleThreeByThreeClick, handleFourByFourClick, handleFiveByFiveClick }) => {
-    return (
-        <div className="menu">
-            <div className="exit-modes" onClick={handleExitClick}>×</div>
-            <h1 className="mode-h1">valitse moodi</h1>
-            <button onClick={handleThreeByThreeClick}>3x3</button>
-            <button onClick={handleFourByFourClick}>4x4</button>
-            <button onClick={handleFiveByFiveClick}>5x5</button>
-        </div>
-    )
-}
+const ModesTab = ({
+  handleExitClick,
+  handleThreeByThreeClick,
+  handleFourByFourClick,
+  handleFiveByFiveClick,
+}) => {
+  return (
+    <div className="menu">
+      <div className="exit-modes" onClick={handleExitClick}>
+        ×
+      </div>
+      <h1 className="mode-h1">valitse moodi</h1>
+      <button onClick={handleThreeByThreeClick}>3x3</button>
+      <button onClick={handleFourByFourClick}>4x4</button>
+      <button onClick={handleFiveByFiveClick}>5x5</button>
+    </div>
+  );
+};
 
-export default ModesTab
+export default ModesTab;
